1e7ee1610460589fee7cf85d1db786aa571d32c0
Brandon Phillips <brandon.phillips976@gmail.com>
2020-10-09 01:02:47 -0700

    fixed data parsing bug for printing notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,20 +6,15 @@ let data = require('../db/db.json')
 
 module.exports = function(app) {
     app.get('/api/notes', function(req, res) {
-        console.log(data)
         res.json(data)
     });
 
     app.post('/api/notes', function(req, res) {
-        console.log(data)
-
         const newNotes = req.body
-        console.log(newNotes)
 
         data.push(req.body)
-        data = JSON.stringify(data, null, 4)
 
-        fs.writeFileSync(path.join(__dirname, '../db/db.json'), data, function(err) {
+        fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(data, null, 4), function(err) {
             if (err) {
                 throw err
             }
